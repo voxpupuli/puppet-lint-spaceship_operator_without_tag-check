11de8f5c37a885b2fc9db26081b5b4c94e07f557
markuszilch <markus.zilch@tik.uni-stuttgart.de>
2023-09-07 22:52:58 +0200

    rubocop save autocorrects and todos

--- lib/puppet-lint/plugins/check_spaceship_operator_without_tag.rb
+++ lib/puppet-lint/plugins/check_spaceship_operator_without_tag.rb
@@ -1,14 +1,14 @@
 PuppetLint.new_check(:spaceship_operator_without_tag) do
   def check
     tokens.each do |t|
-      if t.type == :LCOLLECT and t.next_code_token.type == :RCOLLECT
-        notify :warning, {
-          :message => 'spaceship operator without tag',
-          :line    => t.line,
-          :column  => t.column,
-          :token   => t,
-        }
-      end
+      next unless t.type == :LCOLLECT and t.next_code_token.type == :RCOLLECT
+
+      notify :warning, {
+        message: 'spaceship operator without tag',
+        line: t.line,
+        column: t.column,
+        token: t,
+      }
     end
   end
 end
